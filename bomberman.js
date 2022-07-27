cb63dd96789d5ee91ff687a9d97b7d08b4c0e051
venanciozz <107155465+venanciozz@users.noreply.github.com>
2022-07-27 20:18:03 -0300

    Colisão do vidro

--- bomberman.js
+++ bomberman.js
@@ -19,6 +19,17 @@ Sprite.prototype.centroX = function(){
 Sprite.prototype.centroY = function(){
     return this.y + this.metadeAltura();
 }
+<<<<<<< HEAD
+
+
+
+var tela = document.querySelector("canvas");
+var ctx = tela.getContext("2d");
+
+//teclas
+var LEFT=37, UP=38, RIGHT=39, DOWN=40, SPACE=32;
+
+=======
  
  
  
@@ -28,11 +39,29 @@ var ctx = tela.getContext("2d");
 //teclas
 var LEFT=37, UP=38, RIGHT=39, DOWN=40, SPACE=32;
  
+>>>>>>> bd3196389f948469ed0ca413fc718a6ae4f220e0
 //movimento
 var mvLeft = mvUp = mvRight = mvDown = bomba = false;
 var velocidade = 2;
 //arrays
 var mapa = [
+<<<<<<< HEAD
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,0,0,0,2,0,1,2,2,0,0,2,0,0,1],
+[1,0,1,2,1,0,1,0,1,0,1,0,1,0,1],
+[1,0,0,0,2,0,0,0,0,0,2,0,0,1,1],
+[1,0,1,1,1,0,1,0,1,2,1,0,1,0,1],
+[1,0,0,0,0,2,0,0,0,2,2,2,2,0,1],
+[1,1,1,0,1,2,1,0,1,2,1,2,1,2,1],
+[1,2,0,0,2,0,0,0,2,2,2,0,0,0,1],
+[1,0,1,0,1,2,1,0,1,0,1,0,1,2,1],
+[1,0,1,2,0,1,0,0,2,0,0,2,2,0,1],
+[1,0,1,0,1,0,1,0,1,0,1,0,1,2,1],
+[1,2,2,2,2,0,2,0,0,2,0,0,0,0,1],
+[1,0,1,0,1,0,1,2,1,1,1,0,1,2,1],
+[1,0,2,0,0,2,0,1,0,0,0,0,0,2,1],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]              
+=======
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
     [1,0,0,0,2,0,1,2,2,0,0,2,0,0,1],
     [1,0,1,2,1,0,1,0,1,0,1,0,1,0,1],
@@ -48,11 +77,16 @@ var mapa = [
     [1,0,1,0,1,0,1,2,1,1,1,0,1,2,1],
     [1,0,2,0,0,2,0,1,0,0,0,0,0,2,1],
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]        
+>>>>>>> bd3196389f948469ed0ca413fc718a6ae4f220e0
 ]
 var x;
 var y;
 var xBomba;
+<<<<<<< HEAD
+var yBomba;         
+=======
 var yBomba;        
+>>>>>>> bd3196389f948469ed0ca413fc718a6ae4f220e0
 var sprites = [];
 var paredes = [];
 var paredesD = [];
@@ -67,6 +101,16 @@ for(var linhas in mapa){
         if(bloco === 2){
             x = colunas*50
             y = linhas*50
+<<<<<<< HEAD
+            var paredeD = new Sprite(x, y, 50, 50, imagemParedeD)
+            paredesD.push(paredeD);
+        }
+        
+        paredes.push(parede);
+        
+    }  
+} 
+=======
             var paredeD = new Sprite(x,y,50,50,imagemParedeD)
         }
        
@@ -74,6 +118,7 @@ for(var linhas in mapa){
         paredesD.push(paredeD);
     }  
 }
+>>>>>>> bd3196389f948469ed0ca413fc718a6ae4f220e0
 //entradas
 window.addEventListener("keydown",function (e){
     var key = e.keyCode;
@@ -92,12 +137,21 @@ window.addEventListener("keydown",function (e){
             break;
         case SPACE:
             bomba = true;
+<<<<<<< HEAD
+            xBomba = Math.floor(boneco.centroX()/50)*50; 
+            yBomba = Math.floor(boneco.centroY()/50)*50;
+            this.setTimeout(pararBomba,3000); // usando timeout para fazer a bomba desaparecer.
+            break;           
+    }
+    
+=======
             xBomba = Math.floor(boneco.centroX()/50)*50;
             yBomba = Math.floor(boneco.centroY()/50)*50;
             this.setTimeout(pararBomba,3000); // usando timeout para fazer a bomba desaparecer.
             break;          
     }
    
+>>>>>>> bd3196389f948469ed0ca413fc718a6ae4f220e0
 }, false)
 window.addEventListener("keyup",function (e){
     var key = e.keyCode;
@@ -113,12 +167,21 @@ window.addEventListener("keyup",function (e){
             break;
         case DOWN:
             mvDown = false;
+<<<<<<< HEAD
+            break; 
+                
+    }
+
+}, false)
+
+=======
             break;
                
     }
  
 }, false)
  
+>>>>>>> bd3196389f948469ed0ca413fc718a6ae4f220e0
 //objetos
 var imagemBoneco = new Image();
 imagemBoneco.src ="https://art.pixilart.com/c5e4d357e30cf9d.png";
@@ -133,15 +196,27 @@ imagemParedeD.src = "https://i.imgur.com/O20hO2f.png";
  
 var imagemBomba = new Image();
 imagemBomba.src = "https://opengameart.org/sites/default/files/styles/medium/public/Bomb_anim0001.png"
+<<<<<<< HEAD
+
+//funções 
+=======
 //funções
+>>>>>>> bd3196389f948469ed0ca413fc718a6ae4f220e0
 function loop (){
     window.requestAnimationFrame(loop,tela);
     atualiza();
     desenha();
+<<<<<<< HEAD
+    
+    
+}
+
+=======
     console.log("X = "+boneco.x+" Centro = "+ boneco.centroX())
    
 }
  
+>>>>>>> bd3196389f948469ed0ca413fc718a6ae4f220e0
 function atualiza(){
     if(mvLeft && !mvRight && !mvDown && !mvUp){
         boneco.x -= velocidade;
@@ -155,6 +230,25 @@ function atualiza(){
     if(mvDown && !mvUp){
         boneco.y += velocidade;
     }
+<<<<<<< HEAD
+
+  
+
+    //colisões
+    for(let i in paredes){
+        let prd = paredes[i];
+        colisao(boneco,prd);
+    }
+    for (let i in paredesD) {
+       let prd = paredesD[i];
+       colisao(boneco, prd);
+    }
+    
+}
+
+
+
+=======
  
  
  
@@ -172,15 +266,22 @@ function atualiza(){
  
  
  
+>>>>>>> bd3196389f948469ed0ca413fc718a6ae4f220e0
 function desenha() {
     var x;
     var y;
     ctx.clearRect(0,0,tela.width,tela.height);
     for(var i in sprites){
         var spr = sprites[i];
+<<<<<<< HEAD
+        ctx.drawImage(imagemBoneco,spr.x, spr.y, spr.largura, spr.altura); 
+    }
+
+=======
         ctx.drawImage(imagemBoneco,spr.x, spr.y, spr.largura, spr.altura);
     }
  
+>>>>>>> bd3196389f948469ed0ca413fc718a6ae4f220e0
     for(var linhas in mapa){
         for(var colunas in mapa[linhas]){
             var bloco = mapa[linhas][colunas];
@@ -201,6 +302,21 @@ function desenha() {
         ctx.drawImage(imagemBomba,xBomba,yBomba,50,50);
     }
 }
+<<<<<<< HEAD
+
+function colisao(r1,r2){
+    var catX = r1.centroX() - r2.centroX();
+    var catY = r1.centroY() - r2.centroY();
+
+    //soma das metades
+    var smMetadeLargura = r1.metadeLargura() + r2.metadeLargura();
+    var smMetadeAltura = r1.metadeAltura() + r2.metadeAltura();
+
+    if(Math.abs(catX) < smMetadeLargura && Math.abs(catY) < smMetadeAltura){
+        var diferencaX = smMetadeLargura - Math.abs(catX);
+        var diferencaY = smMetadeAltura - Math.abs(catY);
+
+=======
  
 function colisao(r1,r2){
     var catX = r1.centroX() - r2.centroX();
@@ -214,6 +330,7 @@ function colisao(r1,r2){
         var diferencaX = smMetadeLargura - Math.abs(catX);
         var diferencaY = smMetadeAltura - Math.abs(catY);
  
+>>>>>>> bd3196389f948469ed0ca413fc718a6ae4f220e0
         if(diferencaX >= diferencaY){//colisão por cima ou por baixo
             if(catY > 0){//por cima
                 r1.y += diferencaY;
@@ -228,12 +345,25 @@ function colisao(r1,r2){
             }
         }
     }
+<<<<<<< HEAD
+
+=======
  
+>>>>>>> bd3196389f948469ed0ca413fc718a6ae4f220e0
 }
 // Função para deixar a variável da bomba false, para ser usada no setTimeOut.
 function pararBomba(){
     bomba = false;
 }
+<<<<<<< HEAD
+
+loop();
+
+//imagemParede.src = "https://imgur.com/EkleLlt.png";
+
+   
+//ctx.drawImage(imagemBoneco,x,y,50,50);
+=======
  
 loop();
  
@@ -241,3 +371,4 @@ loop();
  
    
 //ctx.drawImage(imagemBoneco,x,y,50,50);
+>>>>>>> bd3196389f948469ed0ca413fc718a6ae4f220e0
